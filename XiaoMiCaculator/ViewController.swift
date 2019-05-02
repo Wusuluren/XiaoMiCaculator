ecc055a57c88cb2419eed32c18b216a6f7e84771
Wusuluren <1634636348@qq.com>
2019-05-02 23:36:09 +0800

    Initial Commit

---
+++ XiaoMiCaculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XiaoMiCaculator
+//
+//  Created by 123 on 2019/5/2.
+//  Copyright © 2019年 wave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ XiaoMiCaculatorTests/XiaoMiCaculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  XiaoMiCaculatorTests.swift
+//  XiaoMiCaculatorTests
+//
+//  Created by 123 on 2019/5/2.
+//  Copyright © 2019年 wave. All rights reserved.
+//
+
+import XCTest
+@testable import XiaoMiCaculator
+
+class XiaoMiCaculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
